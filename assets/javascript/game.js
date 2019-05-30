4c685b297ac4217221fd9b5c85cc184dd0e220f2
baysik <iambaysik@gmail.com>
2019-05-30 14:55:37 -0600

    added game functionality w/ js and made very minor changes to html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,6 @@
 // number to match RNG
-var mainRng = Math.floor((Math.random() * 120) + 19);
-console.log(blueRng);
+var mainRng = Math.floor((Math.random() * 120));
+console.log(mainRng + "mainrng");
 
 // crystal RNG
 var blueRng = Math.floor((Math.random() * 12) + 1);
@@ -15,30 +15,82 @@ console.log(redRng);
 var yellowRng = Math.floor((Math.random() * 12) + 1);
 console.log(yellowRng);
 
-var counter = 0;
 
+// starting total score
+var totalScore = 0;
 
-// click blue crystal
-$("#blue-crystal").click(function(){
-    $("#total-score").text("Total Score:" + " " + blueRng)
-})
+// wins and loss
+var winCount = 0;
+var lossCount = 0;
 
-// click green crystal
-$("#green-crystal").click(function(){
-    alert(greenRng);
-})
+// initiate game
+function initGame() {
+    assignRandomNumberToMatch();
+}
 
-// click red crystal
-$("#red-crystal").click(function(){
-    alert(redRng);
-})
-
-// click yellow crystal
-$("#yellow-crystal").click(function(){
-    alert(yellowRng);
-})
+initGame()
 
 // Load random number to match between 
-var numberToMatch = $("#number-to-match").text("Number to match:" + " " + mainRng);
+function assignRandomNumberToMatch(){
+    $("#number-to-match").text("Number to match:" + " " + mainRng);
+}
 
 // add every time a crystal is clicked
+blueNumber = parseInt(blueRng);
+greenNumber = parseInt(greenRng);
+redNumber = parseInt(redRng);
+yellowNumber = parseInt(yellowRng);
+
+$("#blue-crystal").click(function(){
+    totalScore = totalScore + blueNumber;
+    $("#total-score").text("Total Score:" + " " + totalScore);
+    if (totalScore === mainRng) {
+        winCount++;
+        $("#win-count").text("Wins: " + winCount);
+        alert("you won!");
+    } else if (totalScore > mainRng){
+        lossCount++
+        $("#loss-count").text("Losses: " + lossCount);
+        alert("you lost!");
+    };
+});
+
+$("#green-crystal").click(function(){
+    totalScore = totalScore + greenNumber;
+    $("#total-score").text("Total Score:" + " " + totalScore);
+    if (totalScore === mainRng) {
+        alert("you won!");
+    } else if (totalScore > mainRng){
+        alert("you lost!");
+    };
+});
+
+$("#red-crystal").click(function(){
+    totalScore = totalScore + redNumber;
+    $("#total-score").text("Total Score:" + " " + totalScore);
+    if (totalScore === mainRng) {
+        alert("you won!");
+    } else if (totalScore > mainRng){
+        alert("you lost!");
+    };
+});
+
+$("#yellow-crystal").click(function(){
+    totalScore = totalScore + yellowNumber;
+    $("#total-score").text("Total Score:" + " " + totalScore);
+    if (totalScore === mainRng) {
+        alert("you won!");
+    } else if (totalScore > mainRng){
+        alert("you lost!");
+    };
+});
+
+// everytime the player wins update the score by 1
+
+if (totalScore == mainRng) {
+    alert("you won!");
+} else if (totalScore > mainRng){
+    alert("you lost!");
+};
+
+// everytime the player loses update the score by 1
